c7610518209bb4f43298ccd630a4975d608effe2
Daniel <daniostan909@gmail.com>
2022-06-03 18:39:28 +0200

    Updates

---
+++ precache-manifest.38402a8527b2996cd48412ff550bad6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c523754a83bb0f892413",
+    "url": "/Combinatorics_Calculator/static/css/main.95f093a7.chunk.css"
+  },
+  {
+    "revision": "c523754a83bb0f892413",
+    "url": "/Combinatorics_Calculator/static/js/main.c523754a.chunk.js"
+  },
+  {
+    "revision": "8b091dac83200d3fff1a",
+    "url": "/Combinatorics_Calculator/static/js/1.8b091dac.chunk.js"
+  },
+  {
+    "revision": "70d95ef3f82c2a652c31",
+    "url": "/Combinatorics_Calculator/static/js/runtime~main.70d95ef3.js"
+  },
+  {
+    "revision": "2b4303e8fa35751371bee09e3be1a499",
+    "url": "/Combinatorics_Calculator/static/media/nWk.2b4303e8.png"
+  },
+  {
+    "revision": "c804e5c1e1c60256a8b6509780567061",
+    "url": "/Combinatorics_Calculator/index.html"
+  }
+];
